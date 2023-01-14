38f50fdfae510cfceed5779d505857d4ec037e6e
potocki92 <mateusz.potocki92@gmail.com>
2023-01-14 18:49:29 +0100

    Install libraries and create first js files
    Install libraries:
        "dotenv": "^16.0.3",
        "express": "^4.18.2",
        "express-flash": "^0.0.2",
        "express-session": "^1.17.3",
        "method-override": "^3.0.0",
        "mongoose": "^6.8.3",
        "node": "^19.4.0",
        "passport": "^0.6.0"
    
    Added server.js and use node to start server

---
+++ server.js
@@ -0,0 +1,69 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+  }
+  
+  // Importing libraries that we installed using npm
+  const express = require("express");
+  const mongoose = require("mongoose");
+  const passport = require("passport");
+  const flash = require("express-flash");
+  const session = require("express-session");
+  const methodOverride = require("method-override");
+  
+  const app = express();
+  
+  // // Passport Config
+  require("./passport-config")(passport);
+  
+  // DB Config
+  const db = require("./config/keys").mongoURI;
+  
+  // Connect to MongoDB
+  mongoose
+    .connect(db)
+    .then(() => console.log("MongoDB Connected..."))
+    .catch((err) => console.log(err));
+  
+  // Bodyparser
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+  
+  // Static Files
+  app.use(express.static("./"));
+  app.set("/views", express.static(__dirname + "/views"));
+  app.use("/css", express.static(__dirname + "/css"));
+  
+  // Connect flash
+  app.use(flash());
+  
+  // Express session
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false, // We won't reasve the session variable if nothing is change
+      saveUninitialized: false,
+    })
+  );
+  
+  // Passport middleware
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+  app.use(methodOverride("_method"));
+  
+  // Global variables
+  app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash("success_msg");
+    res.locals.error_msg = req.flash("error_msg");
+    res.locals.error = req.flash("error");
+    next();
+  });
+  
+  // Routes
+  app.use("/", require("./routes/users"));
+  
+  
+  app.listen(3000, () => {
+    console.log("Server is ready: http://localhost:3000");
+  });
+  
